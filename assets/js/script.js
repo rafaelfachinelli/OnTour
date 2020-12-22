91660ef1ba7f7ccf3cd2bddf200c948f6101a2c0
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-12-22 12:52:49 -0300

    new menu style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
-const menuItemElements = document.querySelectorAll('.menu__item');
+const navElement = document.querySelector('.nav');
+const navMenuElement = document.querySelector('.nav__menu');
 const menuExpandElement = document.querySelector('.menu__expand');
 // console.log(menuItemElements);
 console.log();
 
 menuExpandElement.addEventListener('click', (event) => {
-  const element = event.target;
-
-  for (menuItem of menuItemElements) {
-    menuItem.childNodes[3].classList.toggle('invisible');
-  }
+  
+  navMenuElement.classList.toggle('invisible');
   menuExpandElement.childNodes[1].classList.toggle('flipImage');
 });
